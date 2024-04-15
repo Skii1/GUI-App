94f2615ce021f77dd9246d83902f5fd6d8985105
Alex <alexander.mcisaac@student.tdsb.on.ca>
2024-04-15 10:09:18 -0400

    yes no dialog box with user feedback

--- src/App.java
+++ src/App.java
@@ -2,8 +2,22 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String name = JOptionPane.showInputDialog("Enter Your Name: ");
-        int age = Integer.parseInt(JOptionPane.showInputDialog("enter Your Age : "));
+        int age;
+        String name;
+        int ans = 1;
+
+        name = JOptionPane.showInputDialog("Enter Your Name: ");
+        age = Integer.parseInt(JOptionPane.showInputDialog("enter Your Age : "));
         JOptionPane.showMessageDialog(null, "Hello there " + name + ", you are " + age + " years old!");
+
+        while(ans != 0){
+            ans = JOptionPane.showConfirmDialog(null, "do you use arch?", "btw,", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+                if (ans == 1){
+                    JOptionPane.showMessageDialog(null, "Why not, " + name + ". You should use Arch, y'know...", "What???", JOptionPane.WARNING_MESSAGE);
+                }
+            }
+        JOptionPane.showMessageDialog(null, "Good... The only correct option!");
+        
     }
 }
