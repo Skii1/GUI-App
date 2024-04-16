1b16aac8baff4a1caaa39e11e3d60398e34e74c9
Alex <alexander.mcisaac@student.tdsb.on.ca>
2024-04-16 08:35:21 -0400

    broken save branch, import issues

--- src/App.java
+++ src/App.java
@@ -1,23 +1,43 @@
+import javax.swing.JDesktopPane;
 import javax.swing.JOptionPane;
-
+import java.util.concurrent.TimeUnit;
+import NumberGuess;
 public class App {
     public static void main(String[] args) throws Exception {
         int age;
         String name;
-        int ans = 1;
+        int archAns = 1;
+        int gameReady = 2;
+        int closeTimer = 1;
+        Object[] options = {"Ready", "Not yet", "Maybe Later"};
 
         name = JOptionPane.showInputDialog("Enter Your Name: ");
         age = Integer.parseInt(JOptionPane.showInputDialog("enter Your Age : "));
         JOptionPane.showMessageDialog(null, "Hello there " + name + ", you are " + age + " years old!");
 
-        while(ans != 0){
-            ans = JOptionPane.showConfirmDialog(null, "do you use arch?", "btw,", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        while(archAns != 0){
+            archAns = JOptionPane.showConfirmDialog(null, "do you use arch?", "btw,", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 
-                if (ans == 1){
+                if (archAns == 1){
                     JOptionPane.showMessageDialog(null, "Why not, " + name + ". You should use Arch, y'know...", "What???", JOptionPane.WARNING_MESSAGE);
                 }
             }
         JOptionPane.showMessageDialog(null, "Good... The only correct option!");
-        
+
+        while(gameReady == 1){
+            JOptionPane.showOptionDialog(null, "Let's play a number guessing game. Are you ready?", "Game", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+            if(gameReady == 1){
+                JOptionPane.showMessageDialog(null, "OK. Waiting for you.");
+            }
+        }
+        if(gameReady == 2){
+            JOptionPane.showMessageDialog(null, "Ok, see you later! App closing in : " + closeTimer);
+            TimeUnit.SECONDS.sleep(2);
+            System.exit(0);
+        }
+
+        if(gameReady ==  0){
+            NumberGuess()
+        }
     }
 }
