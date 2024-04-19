ac37b0b03838cbfca9a135cb123ee450b2f2f1ea
Alex <alexander.mcisaac@student.tdsb.on.ca>
2024-04-19 12:00:58 -0400

    Beta release for assignment

--- src/App.java
+++ src/App.java
@@ -1,17 +1,17 @@
 import javax.swing.JOptionPane;
-
 public class App {
     public static void main(String[] args) throws Exception {
         int age;
         String name;
-        int question = 1;
-        int gameReady = 1;
-        Object[] gameOptions = { "Ready", "Not yet", "Maybe Later" }; // gameReady button options object
+        int question = 1; //answer to question for 1 as true and 0 as false, acts as boolean
+        int gameReady = 1; //result from buttons to get ready state for number guessing game
 
+        //name and age prompt
         name = JOptionPane.showInputDialog("Enter Your Name: ");
         age = Integer.parseInt(JOptionPane.showInputDialog("enter Your Age : "));
         JOptionPane.showMessageDialog(null, "Hello there " + name + ", you are " + age + " years old!");
 
+        //true or false question
         while (question != 0) {
             question = JOptionPane.showConfirmDialog(null, "Will UNIX Time ever end?", "True or False?",
                     JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
@@ -24,24 +24,28 @@ public class App {
         JOptionPane.showMessageDialog(null,
                 "That's Right! Unix time will end on January 19th, 2038. This is called the Y2k38 problem.");
 
+        //defining button options for gameReady
+        Object[] gameOptions = { "Ready", "Not yet", "Maybe Later" }; // gameReady button options object
+
         while (gameReady == 1) {
-            //get user choice for game, repeat if user chooses wait option. Can be implemented as do while
+            // get user choice for game, repeat if user chooses wait option. Can be implemented as do while
             gameReady = JOptionPane.showOptionDialog(null, "Let's play a number guessing game. Are you ready?", "Game",
                     JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, gameOptions, gameOptions[0]);
             if (gameReady == 1) {
                 JOptionPane.showMessageDialog(null, "OK. Waiting for you.");
             }
         }
+        //close app
         if (gameReady == 2) {
             JOptionPane.showMessageDialog(null, "Ok, see you later!");
             System.exit(0);
         }
-
+        //run number guessing class
         else if (gameReady == 0) {
             // Run number guessing method
             GuessNumber.main();
         } else {
-            // Skeletion error handling 
+            // Skeletion error handling
             JOptionPane.showMessageDialog(null, "An unknown error occurred.", "Error", JOptionPane.WARNING_MESSAGE);
         }
     }
--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,23 +1,26 @@
 import javax.swing.JOptionPane;
 import java.util.Random;
 public class GuessNumber {
-    public static void main() throws Exception {
-
-        // random number generator
+    public static void main() throws Exception { // Number guessing game method, used in App.java.
+        
+        // Random number generator
         Random random = new Random();
         int numberToGuess = random.nextInt(100) + 1;
-        int guess = Integer.parseInt(JOptionPane.showInputDialog("Pick a number between 1 and 100."));
+        int guess = Integer.parseInt(JOptionPane.showInputDialog("Guess a number between 1 and 100."));
 
+        //loop until number is guessed
         while (guess != numberToGuess) {
             if (guess > numberToGuess) {
                 guess = Integer.parseInt(JOptionPane.showInputDialog("Not Quite. The number is less than " + guess + ".\nGuess again"));
             } else if (guess < numberToGuess) {
                 guess = Integer.parseInt(JOptionPane.showInputDialog("Not Quite. The number is greater than " + guess + ".\nGuess again"));
+            //skeleton error handling
             } else {
                 JOptionPane.showMessageDialog(null, "An unknown error occured.", "Error", JOptionPane.ERROR_MESSAGE);
             }
         }
 
+        //success message
         Object[] options = { "Continue" };
         JOptionPane.showOptionDialog(null, "Congratulations! You guessed the right number, " + guess, "You Won!",
                 JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
